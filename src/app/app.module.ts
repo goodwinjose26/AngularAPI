00911854f250577add8becd464cf2563ab177cb9
goodwinjose26 <josegoodwin22@gmail.com>
2022-11-29 10:17:08 +0530

    navbar,passengerdata,usdata,userinfo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PassengerdataComponent } from './passengerdata/passengerdata.component';
+import { UsdataComponent } from './usdata/usdata.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:PassengerdataComponent
+    
+  },
+  {
+    path:"usdata",
+    component:UsdataComponent
+  },
+  {
+    path:"userinfo",
+    component:UserinfoComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PassengerdataComponent,
+    UsdataComponent,
+    NavbarComponent,
+    UserinfoComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
