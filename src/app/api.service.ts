1200b6fd0906283b82adf3f62bd23641262228e0
goodwinjose26 <josegoodwin22@gmail.com>
2022-11-29 17:18:27 +0530

    all services-passengerdata,product,todo,usdata,userdata,userinfo

---
+++ src/app/api.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+  fetchPassenger =()=>
+  {
+    return this.http.get("https://api.instantwebtools.net/v1/passenger?page=0&size=30")
+  }
+  fetchProduct =()=>
+  {
+    return this.http.get("https://dummyjson.com/products")
+  }
+  fetchTodo =()=>
+  {
+    return this.http.get("https://dummyjson.com/todos")
+  }
+  fetchUsdata =()=>
+  {
+    return this.http.get("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
+  }
+  fetchUserdata =()=>
+  {
+    return this.http.get("https://dummyjson.com/users")
+  }
+  fetchUserinfo =()=>
+  {
+    return this.http.get("https://reqres.in/api/users?page=1")
+  }
+}
